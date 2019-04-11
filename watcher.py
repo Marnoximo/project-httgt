451b725e33db97b8bfe65d7beabf7286e9ac1f47
tqtien <quangtien97dn@gmail.com>
2019-04-12 01:13:09 +0700

    Add RabbitMQ message queue

--- watcher.py
+++ watcher.py
@@ -1,109 +1,20 @@
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import PatternMatchingEventHandler
 
 import time
-import pathlib
-from glob import glob
 import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pika
 
 WATCHED_FOLDER_PATH = './data'
 
-def verbalise(x):
-    if x >= 90:
-        return 'Very good'
-    elif x >= 75:
-        return 'Good'
-    elif x >= 50:
-        return 'Average'
-    elif x >= 20:
-        return 'Bad'
-    elif x >= 1:
-        return 'Very bad'
-    else:
-        return 'None'
-    
-def analyze_metacritic():
-    df = pd.read_csv('./data/metacritic.csv')
-    if df is None:
-        return False
-    
-    df['metascore'] = df['metascore'].apply(lambda x: float(x))
-    df['userscore'] = df['userscore'].apply(lambda x: float(x) if x!='tbd' else 0)
-    m_dict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, \
-          'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
-    df['month'] = df['release_date'].apply(lambda x: x.split(',')[0].split(' ')[0]).map(m_dict)
-    df['day'] = df['release_date'].apply(lambda x: int(x.split(',')[0].split(' ')[2]) if '  ' in x else int(x.split(',')[0].split(' ')[1]))
-    df['year'] = df['release_date'].apply(lambda x: int(x.split(', ')[1]))
-    
-    df.groupby(['platform']).count()['title'].plot.bar()
-    plt.savefig('./static/figures/metacritic/numTitlesByPlatforms.png', bbox_inches='tight')
-    
-    df.groupby(['platform']).mean()['metascore'].plot.bar()
-    plt.savefig('./static/figures/metacritic/averageMetascoreByPlatform.png', bbox_inches='tight')
 
-    df.groupby(['platform']).mean()['userscore'].plot.bar()
-    plt.savefig('./static/figures/metacritic/averageUserscoreByPlatforms.png', bbox_inches='tight')
-    
-    df['metascore_verdict'] = df['metascore'].apply(verbalise)
-    df['userscore_verdict'] = df['userscore'].apply(lambda x: verbalise(x*10))
-    
-    df.groupby(['metascore_verdict']).count()['title'].plot.bar()
-    plt.savefig('./static/figures/metacritic/numTitlesByMetaVerdict.png', bbox_inches='tight')
-    
-    df.groupby(['userscore_verdict']).count()['title'].plot.bar()
-    plt.savefig('./static/figures/metacritic/numTitlesByUserVerdict.png', bbox_inches='tight')
-    
-    return True
-
-def get_verdict(x):
-    if x >= 90:
-        return 'Very good'
-    elif x >= 75:
-        return 'Good'
-    elif x >= 60:
-        return 'Average'
-    elif x >= 45:
-        return 'Bad'
-    else:
-        return "Very bad"
-    
-def analyze_gamerk():
-    df = pd.read_csv('./data/gameranking.csv')
-    if df is None:
-        return False
-
-    df.groupby(['platform']).count()['title'].plot.bar()
-    plt.title('Number of titles by platforms')
-    plt.savefig('./static/figures/gameranking/numTitlesByPlatforms.png', bbox_inches='tight')
-
-    df.groupby(['platform']).mean()['score'].plot.bar()
-    plt.title('Average scores by platforms')
-    plt.savefig('./static/figures/gameranking/averageScoresByPlatforms.png', bbox_inches='tight')
-
-    df['verdict'] = df['score'].apply(get_verdict)
-
-    df.groupby(['verdict']).count()['title'].plot.bar()
-    plt.title('Number of titles by verdict')
-    plt.savefig('./static/figures/gameranking/numTitlesByVerdict.png', bbox_inches='tight')
-    
-    df.groupby(['title']).mean()['score'].sort_values(ascending=False).head(15).plot.bar()
-    plt.title('Top 15 games with highest scores')
-    plt.savefig('./static/figures/gameranking/top15games.png', bbox_inches='tight')
-    
-    return True
-
-def data_handler():
-    print('Got event!')
-    if 'gameranking.csv' in os.listdir('./data'):
-        analyze_gamerk()
-        print('Analyzed gameranking data')
-    if 'metacritic.csv' in os.listdir('./data'):
-        analyze_metacritic()
-        print('Analyzed metacritic data')
+def fire_messages(contents):
+    print('[x] send message: ', contents)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='Tien7559'))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_publish(exchange='', routing_key='hello', body=contents)
+    connection.close()
 
 class Watcher:
 	directory = WATCHED_FOLDER_PATH
@@ -123,10 +34,20 @@ class Watcher:
 			print('Error')
 		self.observer.join()
 
-class Handler(FileSystemEventHandler):
-	@staticmethod
-	def on_any_event(event):
-		data_handler()
+class Handler(PatternMatchingEventHandler):
+    patterns = ["*.csv"]
+    
+    def process (self, event):
+        head, filename = os.path.split(event.src_path)
+        if (filename == 'metacritic.csv'):
+            fire_messages(filename)
+        elif (filename == 'gameranking.csv'):
+            fire_messages(filename)
+        else:
+            fire_messages(filename)
+        
+    def on_modified(self, event):
+        self.process(event)
 
 if __name__ == '__main__':
 	w = Watcher()
